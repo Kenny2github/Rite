d957bfd8f38b9c8e81805fed4cf275a1db4ec859
rjworks <arjayandal05@yahoo.com>
2021-11-07 00:19:39 -0700

    Add modal form, store data to localstorage

--- client/src/modals/Results.js
+++ client/src/modals/Results.js
@@ -1,13 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Results = (props) => {
 
     const [show, setShow] = useState(props.show);
+    const [results, setResults] = useState(props.results);
+
+    const handleConfirmation = (confirmed) => {
+        if(results && confirmed) {
+            if(!localStorage.getItem('data'))
+                localStorage.setItem('data', JSON.stringify([]));
+            else {
+                let data = JSON.parse(localStorage.getItem('data'));
+                data.push({
+                    'total': results.total,
+                    'spending_category': results.spending_category,
+                    'payment_method': results.payment_method
+                });
+                localStorage.setItem('data', JSON.stringify(data));
+                // reset results just in case
+                setResults({});
+            }
+        }
+        setShow(false);
+    }
 
     return (
         <div>
-            {console.log(show)}
-            {show && (
+            {(show && results) && (
                 <div className="flex justify-center h-screen items-center bg-gray-200 antialiased">
                     <div
                         className="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl">
@@ -15,96 +34,50 @@ const Results = (props) => {
                             className="flex flex-row justify-between p-6 bg-white border-b border-gray-200 rounded-tl-lg rounded-tr-lg"
                         >
                             <p className="font-semibold text-gray-800">Summary</p>
-                            <svg
-                                className="w-6 h-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                ></path>
-                            </svg>
+                            <button onClick={() => handleConfirmation(false)}>
+                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                          d="M6 18L18 6M6 6l12 12"/>
+                                </svg>
+                            </button>
                         </div>
                         <div className="flex flex-col px-6 py-5 bg-gray-50">
-                            {/*<p className="mb-2 font-semibold text-gray-700">Bots Message</p>*/}
-                            {/*<textarea*/}
-                            {/*    type="text"*/}
-                            {/*    name=""*/}
-                            {/*    placeholder="Type message..."*/}
-                            {/*    className="p-5 mb-5 bg-white border border-gray-200 rounded shadow-sm h-36"*/}
-                            {/*    id=""*/}
-                            {/*></textarea>*/}
                             <div className="flex flex-col text-lg sm:flex-row items-center mb-5 sm:space-x-5">
                                 <div className="w-full sm:w-1/2">
                                     <p className="mb-2 font-semibold text-gray-700">Total Spent</p>
                                     <p className="w-full text-base p-5 bg-white border border-gray-200 rounded shadow-sm appearance-none">
-                                        Data
+                                        {results.total}
                                     </p>
                                 </div>
                                 <div className="w-full sm:w-1/2">
                                     <p className="mb-2 font-semibold text-gray-700">Shopping Category</p>
                                     <p className="w-full text-base p-5 bg-white border border-gray-200 rounded shadow-sm appearance-none">
-                                        Data
+                                        {results.spending_category}
                                     </p>
                                 </div>
                                 <br/>
                                 <div className="w-full sm:w-1/2 mt-2 sm:mt-0">
                                     <p className="mb-2 font-semibold text-gray-700">Payment Method</p>
                                     <p className="w-full text-base p-5 bg-white border border-gray-200 rounded shadow-sm appearance-none">
-                                        Data
+                                        {results.payment_method}
                                     </p>
                                 </div>
                             </div>
                             <hr/>
-
-                            {/*<div className="flex items-center mt-5 mb-3 space-x-4">*/}
-                            {/*    <input*/}
-                            {/*        className="inline-flex rounded-full"*/}
-                            {/*        type="checkbox"*/}
-                            {/*        id="check1"*/}
-                            {/*        name="check1"*/}
-                            {/*    />*/}
-                            {/*    <label className="inline-flex font-semibold text-gray-400" htmlFor="check1">*/}
-                            {/*        Add a crew</label*/}
-                            {/*    ><br/>*/}
-                            {/*    <input*/}
-                            {/*        className="inline-flex"*/}
-                            {/*        type="checkbox"*/}
-                            {/*        id="check2"*/}
-                            {/*        name="check2"*/}
-                            {/*        checked*/}
-                            {/*    />*/}
-                            {/*    <label className="inline-flex font-semibold text-blue-500" htmlFor="check2">*/}
-                            {/*        Add a specific agent</label*/}
-                            {/*    ><br/>*/}
-                            {/*</div>*/}
-                            {/*<div*/}
-                            {/*    className="flex flex-row items-center justify-between p-5 bg-white border border-gray-200 rounded shadow-sm"*/}
-                            {/*>*/}
-                            {/*    <div className="flex flex-row items-center">*/}
-                            {/*        <img*/}
-                            {/*            className="w-10 h-10 mr-3 rounded-full"*/}
-                            {/*            src="https://randomuser.me/api/portraits/lego/7.jpg"*/}
-                            {/*            alt=""*/}
-                            {/*        />*/}
-                            {/*        <div className="flex flex-col">*/}
-                            {/*            <p className="font-semibold text-gray-800">Xu Lin Bashir</p>*/}
-                            {/*            <p className="text-gray-400">table.co</p>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*    <h1 className="font-semibold text-red-400">Remove</h1>*/}
-                            {/*</div>*/}
                         </div>
                         <div
                             className="flex flex-row items-center justify-between p-5 bg-white border-t border-gray-200 rounded-bl-lg rounded-br-lg"
                         >
-                            <button className="font-semibold text-gray-600">Cancel</button>
-                            <button className="px-4 py-2 text-white font-semibold bg-blue-500 rounded">
+                            <button
+                                className="font-semibold text-gray-600"
+                                onClick={() => handleConfirmation(false)}>
+                                Cancel
+                            </button>
+                            <button
+                                className="px-4 py-2 text-white font-semibold bg-blue-500 rounded"
+                                onClick={() => handleConfirmation(true)}
+                            >
                                 Confirm
                             </button>
                         </div>
--- client/src/partials/HeroHome.js
+++ client/src/partials/HeroHome.js
@@ -5,25 +5,22 @@ import Results from '../modals/Results';
 function HeroHome() {
 
     const [videoModalOpen, setVideoModalOpen] = useState(false);
-    const [results, setResults] = useState({});
+    const [results, setResults] = useState(null);
     const [showResults, setShowResults] = useState(false);
 
     const onFileUpload = (e) => {
         let img = e.target.files[0];
-        console.log(img);
-        // setShowResults
-        // do your thing and call showResults
+        // console.log(img);
         setShowResults(true);
     }
 
     useEffect(() => {
-        setShowResults(showResults);
-        console.log("hi")
+        // setShowResults(showResults);
     }, [showResults]);
 
     return (
         <section className="relative">
-            <Results show={showResults}/>
+            {(showResults && results) && (<Results show={showResults} results={results}/>)}
             {/* Illustration behind hero content */}
             <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
                  aria-hidden="true">
@@ -112,24 +109,6 @@ function HeroHome() {
                                 <svg className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto" width="768"
                                      height="432" viewBox="0 0 768 432" xmlns="http://www.w3.org/2000/svg"
                                      xmlnsXlink="http://www.w3.org/1999/xlink">
-                                    {/*<defs>*/}
-                                    {/*  <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="hero-ill-a">*/}
-                                    {/*    <stop stopColor="#FFF" offset="0%" />*/}
-                                    {/*    <stop stopColor="#EAEAEA" offset="77.402%" />*/}
-                                    {/*    <stop stopColor="#DFDFDF" offset="100%" />*/}
-                                    {/*  </linearGradient>*/}
-                                    {/*  <linearGradient x1="50%" y1="0%" x2="50%" y2="99.24%" id="hero-ill-b">*/}
-                                    {/*    <stop stopColor="#FFF" offset="0%" />*/}
-                                    {/*    <stop stopColor="#EAEAEA" offset="48.57%" />*/}
-                                    {/*    <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />*/}
-                                    {/*  </linearGradient>*/}
-                                    {/*  <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="hero-ill-e">*/}
-                                    {/*    <stop stopColor="#4FD1C5" offset="0%" />*/}
-                                    {/*    <stop stopColor="#81E6D9" offset="25.871%" />*/}
-                                    {/*    <stop stopColor="#338CF5" offset="100%" />*/}
-                                    {/*  </radialGradient>*/}
-                                    {/*  <circle id="hero-ill-d" cx="384" cy="216" r="64" />*/}
-                                    {/*</defs>*/}
                                     <g fill="none" fillRule="evenodd">
                                         <circle fillOpacity=".04" fill="url(#hero-ill-a)" cx="384" cy="216" r="128"/>
                                         <circle fillOpacity=".16" fill="url(#hero-ill-b)" cx="384" cy="216" r="96"/>
